e2c05e21b70421fd05e10097f13b485fc83675d6
Zakaria EL AISSAOUI <zakaria.elaissaoui.insa@gmail.com>
2021-02-07 23:56:24 +0100

    Added correct functionality to POST bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,5 @@
+const Bootcamp = require('../models/Bootcamp');
+
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
@@ -21,11 +23,15 @@ exports.getBootcamp = (req, res, next) => {
 // @desc    Create new bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  Private
-exports.createBootcamp = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    msg: 'Create a new bootcamp',
-  });
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+    res.status(201).json(responseHelper(true, bootcamp, undefined));
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+    });
+  }
 };
 
 // @desc    Update bootcamp
--- server.js
+++ server.js
@@ -17,6 +17,9 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+//body Parser
+app.use(express.json());
+
 //Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -24,6 +27,7 @@ if (process.env.NODE_ENV === 'development') {
 
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
+
 const server = app.listen(PORT, () => {
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
