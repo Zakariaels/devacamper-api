8f7577a0d57aafead49cab4ed109202a53e89a3f
Zakaria EL AISSAOUI <zakaria.elaissaoui.insa@gmail.com>
2021-02-07 22:26:30 +0100

    Added colors to the console | colors package

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dontenv = require('dotenv');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
+const colors = require('colors');
 
 // Load ENVIRONMENT VARIABLES
 dontenv.config({ path: './config/config.env' });
@@ -24,12 +25,14 @@ if (process.env.NODE_ENV === 'development') {
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 const server = app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  );
 });
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
   // close server and exit process
   server.close(() => process.exit(1));
 });
