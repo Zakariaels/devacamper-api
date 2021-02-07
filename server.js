a528d816a776f4b9b1e1e15a5aaeb2780f78b9e9
Zakaria EL AISSAOUI <zakaria.elaissaoui.insa@gmail.com>
2021-02-07 17:26:29 +0100

    Added a 3rd party logger (morgan)

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const dontenv = require('dotenv');
+const morgan = require('morgan');
 
 //Route files
 const bootcamps = require('./routes/bootcamps');
 
 const PORT = process.env.PORT || 5000;
-//Load ENV VARD
+//Load ENV VARS
 dontenv.config({ path: './config/config.env' });
 
 const app = express();
 
+//Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
-
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
