1bd0e5ed83ca7384eb3263497cc844d7a29ef782
Zakaria EL AISSAOUI <zakaria.elaissaoui.insa@gmail.com>
2021-02-07 17:09:59 +0100

    Added router and controllers

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const dontenv = require('dotenv');
 
+//Route files
+const bootcamps = require('./routes/bootcamps');
+
+const PORT = process.env.PORT || 5000;
 //Load ENV VARD
 dontenv.config({ path: './config/config.env' });
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+
+//Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
