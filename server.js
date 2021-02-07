32fc737fd00c027d689c2dce30a03a98058e35af
Zakaria EL AISSAOUI <zakaria.elaissaoui.insa@gmail.com>
2021-02-07 22:22:04 +0100

    Connected the app to the database with Mongoose

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const dontenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
-//Route files
+// Load ENVIRONMENT VARIABLES
+dontenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
+
+// Route files
 const bootcamps = require('./routes/bootcamps');
 
 const PORT = process.env.PORT || 5000;
-//Load ENV VARS
-dontenv.config({ path: './config/config.env' });
 
 const app = express();
 
@@ -18,6 +23,13 @@ if (process.env.NODE_ENV === 'development') {
 
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // close server and exit process
+  server.close(() => process.exit(1));
 });
