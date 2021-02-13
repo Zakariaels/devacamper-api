72159f55a60c5ce40204d54507f2e563b94250b2
Zakaria EL AISSAOUI <zakaria.elaissaoui.insa@gmail.com>
2021-02-13 21:16:37 +0100

    Added sort and select queries

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -8,15 +8,41 @@ const geocoder = require('../utils/geocoder');
 // @access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
   let query;
-  let queryStr = JSON.stringify(req.query);
+
+  const reqQuery = { ...req.query };
+  console.log(reqQuery);
+  const fieldsToRemove = ['select', 'sort'];
+
+  fieldsToRemove.forEach((param) => delete reqQuery[param]);
+
+  let queryStr = JSON.stringify(reqQuery);
+  console.log(reqQuery);
+
   queryStr = queryStr.replace(
     /\b(gt|gte|lt|lte|in)\b/g,
     (match) => `$${match}`
   );
 
+  //finding a resource
   query = Bootcamp.find(JSON.parse(queryStr));
 
+  //select fields
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    console.log(fields);
+    query = query.select(fields);
+  }
+
+  //sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
   const bootcamps = await query;
+
   res.status(200).json({
     success: true,
     count: bootcamps.length,
